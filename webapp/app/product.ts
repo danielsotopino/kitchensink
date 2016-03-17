c0b5366b032263a8c7f3407b209fc03ebb2c5030
Daniel <daniel.soto.pino@gmail.com>
2016-03-16 22:53:35 -0300

    Modified Product Class and added Location class to support the full model

--- webapp/app/product.ts
+++ webapp/app/product.ts
@@ -1,4 +1,10 @@
+import {Location} from './location';
+
 export interface Product {
 	id: string;
 	name: string;
+	description: string;
+	imageUrl: string;
+	price: number;
+	location: Location;
 }
